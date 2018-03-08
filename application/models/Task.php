90a32a35f02b61323c090d0db83a6b7209c18318
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-03-08 13:02:33 -0800

    created unit tests for Task model

--- application/models/Task.php
+++ application/models/Task.php
@@ -18,36 +18,45 @@ class Task extends Entity
     
     public function setTask($value)
     {
+        if (empty($value))
+        {
+            throw new InvalidArgumentException('Task must have a value');
+        }
+        if (strlen($value) > 255)
+        {
+            throw new InvalidArgumentException('Task must be less than 255 characters');
+        }
         $this->task = $value;
     }
     
     public function setPriority($value)
     {
+        if (empty($value))
+        {
+            throw new InvalidArgumentException('Priority must have a value');
+        }
+        if ($value > 3 || $value < 1)
+        {
+            throw new InvalidArgumentException('Priority must be between 1 and 3');
+        }
         $this->priority = $value;
     }
     
     public function setSize($value)
     {
+        if (empty($value))
+        {
+            throw new InvalidArgumentException('Size must have a value');
+        }
         $this->size = $value;
     }
     
     public function setGroup($value)
     {
+        if (empty($value))
+        {
+            throw new InvalidArgumentException('Group must have a value');
+        }
         $this->group = $value;
-    }
-    
-    public function setDeadline($value)
-    {
-        $this->deadline = $value;
-    }
-    
-    public function setStatus($value)
-    {
-        $this->status = $value;
-    }
-    
-    public function setFlag($value)
-    {
-        $this->flag = $value;
     }
 }
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,11 +1,85 @@
 <?php
-
-class CITest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+class CITest extends TestCase
 {
     private $CI;
+    private $task;
     public function setUp()
     {
         // Load CI instance normally
         $this->CI = &get_instance();
+        $this->CI->load->model('task');
+        $this->task = new Task();
+    }
+    
+    function testInvalidEmptyTask()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->task = null;
+    }
+    
+    function testInvalidLongTask()
+    {
+        $this->expectException('InvalidArgumentException');
+        $badValue;
+        for ($i = 0; $i < 256; $i++)
+        {
+            $badValue .= 'a';
+        }
+        $this->task->task = $badValue;
+    }
+    
+    function testValidTask()
+    {
+        $this->task->task = 'valid';
+        $this->assertEquals('valid', $this->task->task);
+    }
+    
+    function testInvalidEmptyPriority()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->priority = null;
+    }
+    
+    function testInvalidHighPriority()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->priority = 4;
+    }
+    
+    function testInvalidLowPriority()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->priority = 0;
+    }
+    
+    function testValidPriority()
+    {
+        $this->task->priority = 1;
+        $this->assertEquals(1, $this->task->priority);
+    }
+    
+    function testInvalidEmptySize()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->size = null;
+    }
+    
+    function testValidSize()
+    {
+        $this->task->size = 1;
+        $this->assertEquals(1, $this->task->size);
+    }
+    
+    function testInvalidEmptyGroup()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->task->group = null;
+    }
+    
+    function testValidGroup()
+    {
+        $this->task->group = 1;
+        $this->assertEquals(1, $this->task->group);
     }
 }
