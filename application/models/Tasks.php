85cda3aea0cdec3a848ed0ce1fb2af6e90eb4178
hscong8 <hscong8@yahoo.com>
2018-03-20 16:49:29 -0700

    Overriding Load() function

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,10 +1,10 @@
 <?php
 
-class Tasks extends CSV_Model {
+class Tasks extends XML_Model {
 
     public function __construct()
     {
-            parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+            parent::__construct(APPPATH . '../data/tasks.xml', 'id');
     }
 
     public function getCategorizedTasks()
@@ -40,7 +40,30 @@ class Tasks extends CSV_Model {
         else
             return 0;
     }
-    
+    protected function load()
+    {
+
+        if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+        {
+            foreach ($tasks as $task) {
+                $record = new stdClass();
+                $record->id = (int) $task->id;
+                $record->task = (string) $task->desc;
+                $record->priority = (int) $task->priority;
+                $record->size = (int) $task->size;
+                $record->group = (int) $task->group;
+                $record->deadline = (string) $task->deadline;
+                $record->status = (int) $task->status;
+                $record->flag = (int) $task->flag;
+
+                $this->_data[$record->id] = $record;
+            }
+        }
+
+        // rebuild the keys table
+        $this->reindex();   
+
+    }
     // provide form validation rules
     public function rules()
     {
